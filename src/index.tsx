0ded3e6cd175286a48e05ebf5aba13c889db1f43
Amit Raikwar <managerarbusiness@gmail.com>
2024-08-11 18:17:45 +0530

    feat(): Adding tanstack-query tool

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 5,
+      retryDelay: 1000,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+);
 
 reportWebVitals();
