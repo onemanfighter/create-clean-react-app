79452ad6eee7bcaba782eff01e6090a2569339a4
Amit Raikwar | OMF <149316922+onemanfighter@users.noreply.github.com>
2024-10-20 01:00:53 +0530

    feat(): Updating minor names (#13)

--- src/screens/landingPage/LandingPage.tsx
+++ src/screens/landingPage/LandingPage.tsx
@@ -1,16 +1,19 @@
 import { TitleBoxContainer } from '@components';
+import { ThemeIcon } from '@components';
 
 const LandingPage = () => {
   return (
     <TitleBoxContainer
-      title={'Hourcoding | Tools'}
+      title={'Template-boilerplate'}
       icon="app"
       display="flex"
       flexDir="column"
       rowGap={10}
       height={'100vh'}
       paddingX={10}
-    ></TitleBoxContainer>
+    >
+      <ThemeIcon />
+    </TitleBoxContainer>
   );
 };
 
